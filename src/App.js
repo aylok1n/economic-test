96bd8935babd15346a740a34d09743f68f2eda4b
aylok1n <nikolya.blog@gmail.com>
2021-12-11 21:45:51 +0400

    Fix styles

--- src/App.js
+++ src/App.js
@@ -2,26 +2,40 @@ import { Container, Tab, Tabs } from '@mui/material'
 import React from 'react';
 import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import Task1 from './Tasks/Task1'
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#FFF',
+        },
+    },
+});
+
 
 function App() {
-  const location = useLocation();
+    const location = useLocation();
 
-  return (
-    <Container maxWidth="sm">
-      <Tabs variant="scrollable" scrollButtons="auto" value={location.pathname}>
-        <Tab label='Основные фонды предприятия' component={Link} to={`/Task1`} value={`/Task1`} />
-        <Tab label='Еще тест' component={Link} to={`/Task2`} value={`/Task2`} />
-      </Tabs>
-      <div >
-        <Routes>
-          <Route path={`/Task1`} element={<Task1 />} />
-          <Route path={`/Task2`}>
-            {/* <UserEventsDataTable /> */}
-          </Route>
-        </Routes>
-      </div>
-    </Container>
-  );
+    return (
+        <>
+            <div style={{ backgroundColor: '#FFF', width: '100vw' }}>
+                <Tabs variant="scrollable" scrollButtons="auto" value={location.pathname}>
+                    <Tab label='Основные фонды предприятия' component={Link} to={`/Task1`} value={`/Task1`} />
+                    <Tab label='Еще тест' component={Link} to={`/Task2`} value={`/Task2`} />
+                </Tabs>
+            </div>
+            <Container maxWidth="sm">
+                <div>
+                    <Routes>
+                        <Route path={`/Task1`} element={<Task1 />} />
+                        <Route path={`/Task2`}>
+                            {/* <UserEventsDataTable /> */}
+                        </Route>
+                    </Routes>
+                </div>
+            </Container>
+        </>
+    );
 }
 
 export default App;
--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,18 +1,31 @@
 import React from 'react'
+import './index.css';
 import { FormControl, FormLabel, Radio, RadioGroup, FormControlLabel } from '@mui/material'
+import { blue, } from '@mui/material/colors';
 
 export const Question = ({ question, answer, options, onCheck }) => {
 
     return (
         <div className='question'>
             <FormControl component="fieldset">
-                <FormLabel component="legend">{question}</FormLabel>
+                <h1>{question}</h1>
                 <RadioGroup
                     name="radio-buttons-group"
                 >
                     {options.map((i, index) => (
                         // <p key={index} ><input name="ans" value={index} type='radio' onChange={(e) => handleCheck(e, index)} />{i}</p>
-                        <FormControlLabel key={index} value={index} control={<Radio />} label={i} onChange={() => onCheck(index)} />
+                        <FormControlLabel
+                            key={index}
+                            value={index}
+                            control={<Radio sx={{
+                                color: blue[50],
+                                '&.Mui-checked': {
+                                    color: blue[200],
+                                },
+                            }} />}
+                            label={<span className=''>{i}</span>}
+                            onChange={() => onCheck(index)}
+                        />
                     ))}
                 </RadioGroup>
             </FormControl>
