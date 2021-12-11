e8aafeca42ba5914d819fd2e118830d7667c77a0
aylok1n <nikolya.blog@gmail.com>
2021-12-11 23:04:31 +0400

    Add watch result

--- src/Tasks/Task1.js
+++ src/Tasks/Task1.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Question from '../Components/Question'
-import { Container } from '@mui/material'
+import { Container, Button } from '@mui/material'
+import './index.css';
 
 const data = require('../data/task1.json')
 
 export const Task1 = (props) => {
     const [show, setShow] = React.useState(1)
     const [answers, setAnswers] = React.useState({})
+    const [result, setResult] = React.useState(false)
 
     const onCheck = (index, value) => {
         if (show < index + 2) setShow(index + 2)
@@ -15,16 +17,30 @@ export const Task1 = (props) => {
         setAnswers(answ)
     }
 
+    const save = () => {
+        let res = 0
+        Object.entries(answers).map(([key, value]) => {
+            !!(data[key].answer === value) && res++
+        })
+        setResult(res)
+    }
+
     return (
-        <div style={{ marginTop: 20 }}>
+        <div style={{ marginTop: 20 }} >
             <Container maxWidth="sm">
                 {
                     data.slice(0, show).map((i, index) => (
                         <Question key={index} {...i} onCheck={(val) => onCheck(index, val)} />
                     ))
                 }
+                {
+                    !!(show === 11) && <div style={{ display: 'flex', justifyContent: 'center', marginBottom: 80 }}>
+                        <Button onClick={save} variant="contained">Результат</Button>
+                    </div>
+                }
+                {!!result && <h4>Вы набрали {result}/{data.length} баллов</h4>}
             </Container>
-        </div>
+        </div >
     )
 
 }
